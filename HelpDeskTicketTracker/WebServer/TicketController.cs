5fafee1ccb2c4bda686d5b98afab0ae7f08b2761
John P Aguirre <john.p.aguirre@norcrossvoip.com>
2014-11-10 23:01:02 -0500

    Added bootstrap and created initial index page.  WebAPI is still being tested.

---
+++ HelpDeskTicketTracker/WebServer/TicketController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace HelpDeskTicketTracker.WebServer
+{
+    public class TicketController : ApiController
+    {
+        public Tickets Get()
+        {
+            return new Tickets()
+            {
+                request = 10,
+                incidents = 50
+            };
+        }
+    }
+
+    public class Tickets
+    {
+        public int request;
+        public int incidents;
+
+    }
+}
